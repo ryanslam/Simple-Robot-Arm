7725a3f216635e3e247e75ee8b2cf7636d674acd
Ryan Lam <Ryanshiau1@gmail.com>
2022-07-13 12:05:19 -0700

    Fixed the error with the workspace demo limits.

--- Simple_Robo_Arm/rsl/rr_two_link/static/scripts/forward_kin.js
+++ Simple_Robo_Arm/rsl/rr_two_link/static/scripts/forward_kin.js
@@ -23,11 +23,12 @@ function workspace_demo() {
     console.log("Client sending command WORKSPACE_DEMO")
     theta_1_min = document.getElementById("theta_1_min_workspace").value
     theta_1_max = document.getElementById("theta_1_max_workspace").value
-    if(parseFloat(theta_1_max) <= 90 || parseFloat(theta_1_max) >= -90 || parseFloat(theta_1_min) >= -90 || parseFloat(theta_1_min) <= 90){
-        document.getElementById("guessed_solution").innerHTML = "";
-        socket.emit("workspace_demo", theta_1_min, theta_1_max);
+    if(parseFloat(theta_1_max) > 90 || parseFloat(theta_1_max) < -90 || parseFloat(theta_1_min) > 90 || parseFloat(theta_1_min) < -90){
+        document.getElementById("guessed_solution").innerHTML = "Error, the max and minimum values lie within the range of (-90,90)";
     }
     else{
-        document.getElementById("guessed_solution").innerHTML = "Error, the max and minimum values lie within the range of (-100,100)";
+        document.getElementById("guessed_solution").innerHTML = "";
+        socket.emit("workspace_demo", theta_1_min, theta_1_max);
+        
     }
 }
