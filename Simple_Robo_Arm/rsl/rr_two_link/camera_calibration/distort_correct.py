f7d1e95f10ab6dca309bb19e3f9ea8912c2406d9
ryanslam <Ryanshiau1@gmail.com>
2022-07-18 14:37:46 -0700

    Beginning camera calibration.

--- Simple_Robo_Arm/rsl/rr_two_link/camera_calibration/distort_correct.py
+++ Simple_Robo_Arm/rsl/rr_two_link/camera_calibration/distort_correct.py
@@ -1,3 +1,2 @@
 import cv2
 assert cv2.__version__[0] == '3'
-import picamera
--- Simple_Robo_Arm/rsl/rr_two_link/camera_calibration/take_cal_image.py
+++ Simple_Robo_Arm/rsl/rr_two_link/camera_calibration/take_cal_image.py
@@ -1,6 +1,7 @@
+from picamera import PiCamera
+from picamera.array import PiRGBArray
 import cv2
 import os
-import picamera
 import time
 
 # File path to save the calirbation images.
@@ -12,5 +13,19 @@ camera.start_preview()
 
 date_string = time.strftime("%Y-%m-%d-%H:%M")
 
-sleep(5)
-camera.capture(path + date_string)
+raw_capture = PiRGBArray(camera, size=(480,480))
+
+time.sleep(2)
+
+for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+    img = frame.array
+    cv2.imshow("Frame", img)
+
+    key = cv2.waitKey(0)
+    
+    if key == ord("q"):
+        cv2.imwrite(path + date_string, img)
+    elif key== 27:
+        break
+
+    raw_capture.truncate(0)
