3c1bf38de8556f6e13651728e04e12cd10a7711a
Ryan Lam <Ryanshiau1@gmail.com>
2022-07-18 15:13:16 -0700

    Added calibration images and corresponding image capturing software.

--- Simple_Robo_Arm/rsl/rr_two_link/camera_calibration/take_cal_image.py
+++ Simple_Robo_Arm/rsl/rr_two_link/camera_calibration/take_cal_image.py
@@ -5,27 +5,33 @@ import os
 import time
 
 # File path to save the calirbation images.
-path = "./calibration_images"
+path = "./calibration_images/"
 
-camera = picamera.PiCamera()
-camera.resolution(480,480)
+# Initialize the pi camera module with a resolution (480,480)
+camera = PiCamera()
+camera.resolution = (480,480)
 camera.start_preview()
 
-date_string = time.strftime("%Y-%m-%d-%H:%M")
-
+# Initialize the capture array.
 raw_capture = PiRGBArray(camera, size=(480,480))
 
-time.sleep(2)
+time.sleep(2)           # Camera bootup time.
 
+# Continuously capture the camera frames.
 for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
     img = frame.array
+    # Display the image using cv2
     cv2.imshow("Frame", img)
 
     key = cv2.waitKey(0)
     
+    # Store frame as calibration image.
     if key == ord("q"):
-        cv2.imwrite(path + date_string, img)
+        date_string = time.strftime("%Y-%m-%d-%H:%M:%S")
+        cv2.imwrite(path + date_string +'.jpg', img)
+    # Exit from the capturing loop.
     elif key== 27:
         break
-
+    
+    # Traverse to the next frame.
     raw_capture.truncate(0)
