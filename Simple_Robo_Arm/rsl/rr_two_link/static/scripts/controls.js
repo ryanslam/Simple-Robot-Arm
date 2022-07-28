3a0bca5d53136df9371de9864afb9dceffa82f74
Ryan Lam <Ryanshiau1@gmail.com>
2022-07-27 20:40:41 -0700

    Added controls.

--- Simple_Robo_Arm/rsl/rr_two_link/static/scripts/controls.js
+++ Simple_Robo_Arm/rsl/rr_two_link/static/scripts/controls.js
@@ -7,8 +7,24 @@ function set_active_controller() {
 function set_controller_gains() {
     console.log("Client sending command SET_CONTROLLER_GAINS")
     motor_id = document.getElementById("motor_id").value
+    ki_check = document.getElementById("check_enable_kis")
     kp = document.getElementById("kp").value
-    // ki = document.getElementById("ki").value
+    ki = document.getElementById("ki").value
     kd = document.getElementById("kd").value
-    socket.emit("set_controller_gains", motor_id, kp, 0, kd)
+    if(ki_check){
+        socket.emit("set_controller_gains", motor_id, kp, ki, kd)
+    }
+    else{
+        socket.emit("set_controller_gains", motor_id, kp, 0, kd)
+    }
+}
+
+function set_joint_position() {
+    console.log("Client sending command MOVE_JOINTS_ABSOLUTE")
+    t1 = document.getElementById("theta1").value
+    t2 = document.getElementById("theta2").value
+    use_traj = ""
+    traj_time = "0"
+    socket.emit("set_joint_position", use_traj, traj_time, t1, t2);
+
 }
